d09453624f7186ce60bbb18f842e67147844e07f
ogu89 <kawakami.a@ctw.inc>
2024-01-24 17:22:26 +0900

    feat: assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <div class="Main">Main Page</div>
+  <div class="Main">Hello Nuxt</div>
+  <!-- <img src="/1.jpg" alt="Discover Nuxt 3" /> -->
+  <!-- <img src="/2.jpeg" alt="handlling" /> -->
+  <IconsBell />
 </template>
