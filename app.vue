0fb1a04f388c248f8885fd873585d085a27e43b0
ogu89 <kawakami.a@ctw.inc>
2024-01-24 18:17:17 +0900

    feat: composables

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="bg-slate-900 h-screen text-white">
-    <NuxtLayout>
-      <!-- <Header /> -->
-      <NuxtPage />
-    </NuxtLayout>
+    <!-- <NuxtLayout> -->
+    <!-- <Header /> -->
+    <!-- <NuxtPage /> -->
+    <!-- </NuxtLayout> -->
+    <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
-<template>
+<!-- <template>
   <div class="Main">Hello Nuxt</div>
-  <!-- <img src="/1.jpg" alt="Discover Nuxt 3" /> -->
-  <!-- <img src="/2.jpeg" alt="handlling" /> -->
+  <img src="/1.jpg" alt="Discover Nuxt 3" />
+  <img src="/2.jpeg" alt="handlling" />
   <IconsBell />
+</template> -->
+
+<script setup>
+const { sayHello } = useUtils();
+sayHello();
+</script>
+
+<template>
+  <div class="container py-3 mx-auto Main">
+    <h1 class="text-teal-500">Hello Nuxt 3</h1>
+  </div>
 </template>
