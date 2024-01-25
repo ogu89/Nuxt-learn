500a2165dfdfd398bcc29f424a7e86b96735b841
ogu89 <kawakami.a@ctw.inc>
2024-01-25 15:13:00 +0900

    feat: modules

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="container py-3 mx-auto Main">
+    <h1 class="text-teal-500">Hello Nuxt 3</h1>
+  </div>
+</template>
+
 <!-- <template>
   <div class="Main">Hello Nuxt</div>
   <img src="/1.jpg" alt="Discover Nuxt 3" />
@@ -5,6 +13,7 @@
   <IconsBell />
 </template> -->
 
+<!-- plguins -->
 <!-- <script setup>
 const { sayHello } = useUtils();
 sayHello();
@@ -12,14 +21,16 @@ const { $sayHello } = useNuxtApp();
 $sayHello("hye");
 </script> -->
 
-<script setup>
+<!-- middleware -->
+<!-- <script setup>
 definePageMeta({
   middleware: "auth",
 });
-</script>
+</script> -->
 
-<template>
+<!-- modules -->
+<!-- <template>
   <div class="container py-3 mx-auto Main">
-    <h1 class="text-teal-500">Hello Nuxt 3</h1>
+    <ContentDoc />
   </div>
-</template>
+</template> -->
